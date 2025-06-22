31a583c55aa6f09e0af9391d5c8cd4fb68488e0e
Mark Klimov <markklimoff@gmail.com>
2025-06-22 17:50:26 +0500

    Исправил генерацию ID в helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,5 @@
 function generateId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+    return Date.now().toString() + Math.random().toString(36).substr(2, 5);
 }
 
 function validateTaskData(data) {
